e4437159a891820b419e474e50fbe11c551bb488
shahrukh mvc crud <mushahrukh.khan@gmail.com>
2019-05-01 12:53:48 -0700

    First Commit

---
+++ WebAPICrud/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPICrud.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET: api/Student
+        public IEnumerable<Models.Generic2dModel> Get()
+        {
+            DAL.WebAPiEntities entities = new DAL.WebAPiEntities();
+            var result = entities.Classes.Select(s => new Models.Generic2dModel() {
+                    Id=s.Id,
+                    Name=s.Name
+            }).ToList();
+            return result; 
+        }
+
+        // GET: api/Student/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Student
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Student/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Student/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
